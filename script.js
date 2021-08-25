799cb3acbcb37f8bfe7f890e4a3b792619a133c6
ezza022 <alfirman.pahlepi@gmail.com>
2021-08-25 18:56:42 +0700

    validate

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const screen = document.querySelector(".screen");
-screen.innerHTML = ""
+screen.innerHTML = "";
 
-const addValue = (value) => (screen.innerHTML += value);
+const addValue = (value, validate) => {
+  if (validate(value)) screen.innerHTML += value;
+};
 
-const calculate = () => (screen.innerHTML = new Function("return " + screen.innerHTML)());
+const calculate = () => {
+  if (screen.innerHTML) screen.innerHTML = new Function("return " + screen.innerHTML)();
+};
 
 const clearAll = () => (screen.innerHTML = "");
 
-const backspace = () => (screen.innerHTML = screen.innerHTML.slice(0,-1));
+const backspace = () => (screen.innerHTML = screen.innerHTML.slice(0, -1));
+
+const validate = (value) => {
+  const length = screen.innerHTML.length;
+
+  const parseValue = parseInt(value);
+  const lastScreenValue = parseInt(screen.innerHTML[length - 1]);
+
+  const isNumber = !isNaN(parseValue);
+  const isTwiceOperator = !isNumber === isNaN(lastScreenValue);
+
+  // screen sedang kosong value pertama harus angka
+  // screen tidak kosong operator tidak boleh diinput 2x
+  if ( isNumber || (screen.innerHTML && !isTwiceOperator))
+    return true;
+
+  return false;
+};
