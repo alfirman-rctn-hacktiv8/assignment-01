df88e79841cc2b4156ea95fdf3e4e9412a043b8e
ezza022 <alfirman.pahlepi@gmail.com>
2021-08-27 17:58:32 +0700

    validate comma

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const screen = document.querySelector(".screen");
 screen.innerHTML = "";
+let hasComma = false
 
 const addValue = (value, validate) => {
   if (validate(value)) screen.innerHTML += value;
@@ -16,16 +17,22 @@ const backspace = () => (screen.innerHTML = screen.innerHTML.slice(0, -1));
 const validate = (value) => {
   const length = screen.innerHTML.length;
 
+  operators = ["+","-","*","/"]
+
   const parseValue = parseInt(value);
   const lastScreenValue = parseInt(screen.innerHTML[length - 1]);
 
   const isNumber = !isNaN(parseValue);
-  const isTwiceOperator = !isNumber === isNaN(lastScreenValue);
+  const isPrevNaN = !isNumber === isNaN(lastScreenValue);
 
   // screen sedang kosong value pertama harus angka
   // screen tidak kosong operator tidak boleh diinput 2x
-  if ( isNumber || (screen.innerHTML && !isTwiceOperator))
-    return true;
+  // koma tidak boleh 2x sebelum operator
 
+  if ( isNumber || (screen.innerHTML && value === "." && !hasComma && !isPrevNaN) || (screen.innerHTML && !isPrevNaN && value !== '.' )){
+    if(value==="." && !hasComma) hasComma = true
+    if(operators.some((operator=>operator===value)) ) hasComma = false
+    return true;
+  }
   return false;
 };
